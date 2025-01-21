67facd78c82a4702a0c94172052265e280bf8233
Jefferson <jeffabuy@gmail.com>
2025-01-21 18:08:02 +0200

    added fetch of items

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,39 @@
+import { useState, useEffect } from "react";
 import Header from "./components/Header";
+import Day from "./components/main/Day";
+import axios from "axios";
 
 function App() {
-  return <Header />;
+  const [items, setItems] = useState([]);
+  const [dataIsLoaded, setDataIsLoaded] = useState(false);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Athens?key=S48BZGXL26MK2UURT4QKZ46NT"
+      )
+      .then((res) => {
+        setItems(res.data);
+        setDataIsLoaded(true);
+      });
+  }, []);
+
+  console.log(items);
+
+  return (
+    <>
+      <Header />
+      {dataIsLoaded ? (
+        <Day
+          up={items.days[0].tempmax}
+          down={items.days[0].tempmin}
+          main={items.currentConditions.temp}
+        />
+      ) : (
+        <h1>wait..</h1>
+      )}
+    </>
+  );
 }
 
 export default App;
--- src/components/main/day.jsx
+++ src/components/main/day.jsx
@@ -1,3 +1,14 @@
-function Day() {
-  return <></>;
+function Day(props) {
+  return (
+    <div>
+      <img />
+      <div>
+        <h1>{props.up}</h1>
+        <h1>{props.down}</h1>
+      </div>
+      <h1>{props.main}</h1>
+    </div>
+  );
 }
+
+export default Day;
