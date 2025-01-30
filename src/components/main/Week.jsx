5283cb62848e0071e1c5849cfc4cb89141160261
Jefferson <jeffabuy@gmail.com>
2025-01-30 19:21:56 +0200

    added the dates and fixed position

--- src/components/main/Week.jsx
+++ src/components/main/Week.jsx
@@ -3,19 +3,23 @@ import Card from "../Card";
 function Week(props) {
   const myArray = props.array;
 
-  console.log(myArray);
-
   return (
-    <div className="flex gap-2 overflow-hidden">
+    <div className="w-full flex gap-2 items-center justify-center">
       {myArray.slice(0, 7).map((item, index) => {
+        console.log(item.datetime.split("-"));
+        const date = item.datetime.split("-");
+
         return (
-          <Card key={index}>
-            <img
-              className="max-w-20"
-              src={"./WeatherPack/" + item.icon + ".png"}
-            />
-            <h2>{item.temp}</h2>
-          </Card>
+          <div className="flex flex-col items-center">
+            <h2>{date[1] + "/" + date[2]}</h2>
+            <Card key={index}>
+              <img
+                className="max-w-20"
+                src={"./WeatherPack/" + item.icon + ".png"}
+              />
+              <h2>{item.temp}</h2>
+            </Card>
+          </div>
         );
       })}
     </div>
