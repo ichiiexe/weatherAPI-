bf54bb66037f4b0f8632f94005b480107eff32fd
Jefferson <jeffabuy@gmail.com>
2025-01-31 05:02:25 +0200

    almost done?

--- src/components/main/Week.jsx
+++ src/components/main/Week.jsx
@@ -4,7 +4,10 @@ function Week(props) {
   const myArray = props.array;
 
   return (
-    <div className="w-full flex gap-2 items-center justify-center">
+    <div className="w-[clamp(100%, 50%, 100%)] flex gap-2 items-center justify-center relative">
+      <h1 className="backgroundText absolute text-[10rem] opacity-15 tracking-widest">
+        7-Day Forecast
+      </h1>
       {myArray.slice(0, 7).map((item, index) => {
         console.log(item.datetime.split("-"));
         const date = item.datetime.split("-");
